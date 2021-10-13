cced3bec247ff74f9de1ee6a0dcf733c1e3c730d
SlightlySasie <53900768+SlightlySasie@users.noreply.github.com>
2021-10-13 23:40:32 +0200

    Delete StyleSheet directory

--- StyleSheet/Javascript.js
+++
@@ -1,8 +0,0 @@
-$('#header a').click(function (e) {
-    e.preventDefault();
-    
-    var offset = $('[name=' + $(this).attr('href').substr(1) + ']').offset();
-    
-    $('html, body').animate({ scrollTop: offset.top - $('#header').outerHeight() }, 'fast');
-    
-});
